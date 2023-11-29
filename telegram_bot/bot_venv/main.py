3b5cfb3b0fa28a8b438959cc287c1b3f4c3d8e77
Molokota <molokota2018@gmail.com>
2023-11-29 15:12:04 +0600

    first commit

---
+++ telegram_bot/bot_venv/main.py
@@ -0,0 +1,14 @@
+import telebot, env
+
+env.BOT_TOKEN
+bot = telebot.TeleBot(env.BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hello World!')
+
+@bot.message_handler(commands=['info'])
+def start(message):
+    bot.send_message(message.chat.id, 'Информация о боте')
+
+bot.polling()
